64a08c2c6d6db93e9f3021a84e3f082339227c52
GregRRoss <grross1@asu.edu>
2026-02-14 09:14:39 -0800

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -22,21 +22,47 @@ func main() {
 	 	os.Exit(1)
 	 }
 	
-	 responder, err = l.Accept()
+	 responder, err = l.Accept() // null pointer exception exists if responder is not set to l.accept
 	 if err != nil {
 	 	fmt.Println("Error accepting connection: ", err.Error())
 	 	os.Exit(1)
 	 }
-
+	// Read get request
 	buffer := make([]byte, 1024) // Create 'Slice' i.e. arrayList needed for net pkg
-	responder.Read(buffer)
-
+	var number_of_bytes_read int
+	number_of_bytes_read, _ = responder.Read(buffer)
+	read_result := string(buffer[:number_of_bytes_read])
+ 	fmt.Println("START OF TRANSMISSION")	
+	fmt.Println(read_result)	
+	fmt.Println("END OF TRANSMISSION")
+	// make response
 	response := "HTTP/1.1 " // Status Line
-	response += "200 " // Status Code
-	response += "OK" // REASON PHRASE 
+	urlFound := true
+
+	// Parse read bytes
+        	// We are looking for what comes after the GET / and before the HTTP/1.1	
+		// Slice we want starts at 01234... character 5 must be a space
+	if read_result[5]==' ' {
+		urlFound = true
+	} else {
+		urlFound = false
+	}
+	// customize response
+	if urlFound {
+		response += "200 " // Status Code
+		response += "OK" // REASON PHRASE
+	} else {
+		response += "404 " // NOT FOUND STATUS CODE
+		response += "Not Found" // REASON PHRASE NOT FOUND	
+	}
+
+	// End response 
         response += "\r\n" // CRLF end of status line
 	response += "\r\n" // CRLF end of headers
 	responder.Write([]byte(response))
 
-
+	// Close resources at end
+	l.Close()
+	responder.Close()
+	// Should actually probably use defer for the above two so they still run if errors out?
 }
